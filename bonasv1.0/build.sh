a2d15b13f723a93c4bac3a02ea2629186a7fde9b
bowen <bowen.nan@icloud.com>
2019-09-04 13:29:43 +0000

    fix

--- bonasv1.0/build.sh
+++ bonasv1.0/build.sh
@@ -3,9 +3,9 @@
 docker stop bonas.nextcloud
 docker rm bonas.nextcloud
 
-docker stop bonas.db
-docker rm bonas.db
+docker stop bonas.mairadb
+docker rm bonas.mariadb
 
-docker run -id --restart=always --name=bonas.db -v /usr/local/bonas/db:/var/liblib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=320000 -e MYSQL_DATABASE=bonas -e MYSQL_USER=bonasdbusr -e MYSQL_PASSWORD=320000 --privileged=true  mariadb 
+docker run -id --restart=always --name=bonas.mariadb -v /usr/local/bonas/db:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=320000 -e MYSQL_DATABASE=bonas -e MYSQL_USER=admin -e MYSQL_PASSWORD=320000 --privileged=true  mariadb 
 docker run -d --restart=always --name=bonas.nextcloud  --link=bonas.db:db -p  80:80 -v /usr/local/bonas/data:/data docker.io/nextcloud
 
